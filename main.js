427235bb5979fb252ad6977e1be7251080a31af7
Luis <roronoazoro3704@gmail.com>
2025-03-16 15:40:16 -0500

    feat: :sparkles: theme completed

--- main.js
+++ main.js
@@ -18,7 +18,34 @@ document.getElementById("menu").addEventListener("click", function() {
 });
 changeSpanish();
 
+var cambioTema=0;
+document.getElementById("tema").addEventListener("click", function() {
+    
+    if (cambioTema===0) {
+        document.documentElement.style.setProperty('--color-text','#fff')
+        document.documentElement.style.setProperty('--color-back','#000')
+        document.documentElement.style.setProperty('--color-sombra','#303030')
+        document.getElementById("FondoPrincipal").style.background=`url("/img/image.png") repeat 0 0`
+        document.getElementById("FondoPrincipal").style.backgroundSize="50px"
+        document.getElementById("tema").innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" viewBox="0 0 24 24"><path fill="var(--color-text)" d="M11.712 3.45a.75.75 0 0 0-.668-1.197c-5.414.494-8.436 4.752-8.764 9.105c-.328 4.361 2.037 8.975 7.451 10.166c5.686 1.25 11.472-2.837 12.016-8.646a.75.75 0 0 0-1.189-.676c-2.837 2.069-6.08 1.316-8.136-.724c-2.054-2.039-2.8-5.239-.71-8.028"/></svg>`
+        cambioTema=1;
+        
+    } else {
+        
+        document.documentElement.style.setProperty('--color-text','#000')
+        document.documentElement.style.setProperty('--color-back','#fff')
+        document.documentElement.style.setProperty('--color-sombra','#edebf8')
+        document.getElementById("FondoPrincipal").style.background=`url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIGNIUk0AAHolAACAgwAA+f8AAIDpAAB1MAAA6mAAADqYAAAXb5JfxUYAAABnSURBVHja7M5RDYAwDEXRDgmvEocnlrQS2SwUFST9uEfBGWs9c97nbGtDcquqiKhOImLs/UpuzVzWEi1atGjRokWLFi1atGjRokWLFi1atGjRokWLFi1af7Ukz8xWp8z8AAAA//8DAJ4LoEAAlL1nAAAAAElFTkSuQmCC") repeat 0 0`
+        document.getElementById("FondoPrincipal").style.backgroundSize="auto"
+        document.getElementById("tema").innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 24 24"><path fill="var(--color-text)" d="M18 12a6 6 0 1 1-12 0a6 6 0 0 1 12 0"/><path fill="var(--color-text)" fill-rule="evenodd" d="M12 1.25a.75.75 0 0 1 .75.75v1a.75.75 0 0 1-1.5 0V2a.75.75 0 0 1 .75-.75M4.399 4.399a.75.75 0 0 1 1.06 0l.393.392a.75.75 0 0 1-1.06 1.061l-.393-.393a.75.75 0 0 1 0-1.06m15.202 0a.75.75 0 0 1 0 1.06l-.393.393a.75.75 0 0 1-1.06-1.06l.393-.393a.75.75 0 0 1 1.06 0M1.25 12a.75.75 0 0 1 .75-.75h1a.75.75 0 0 1 0 1.5H2a.75.75 0 0 1-.75-.75m19 0a.75.75 0 0 1 .75-.75h1a.75.75 0 0 1 0 1.5h-1a.75.75 0 0 1-.75-.75m-2.102 6.148a.75.75 0 0 1 1.06 0l.393.393a.75.75 0 1 1-1.06 1.06l-.393-.393a.75.75 0 0 1 0-1.06m-12.296 0a.75.75 0 0 1 0 1.06l-.393.393a.75.75 0 1 1-1.06-1.06l.392-.393a.75.75 0 0 1 1.061 0M12 20.25a.75.75 0 0 1 .75.75v1a.75.75 0 0 1-1.5 0v-1a.75.75 0 0 1 .75-.75" clip-rule="evenodd"/></svg>`
+        cambioTema=0;
+    }
+
+});
+
 function changeSpanish() {
+    
+
     // Cambio de idioma
     document.querySelector(".espanol").innerHTML = "Español";
     document.querySelector(".ingles").innerHTML = "Inglés";
@@ -107,27 +134,27 @@ function changeEnglish() {
 }
 
 function changeFrench() {
-    // Changement de langue
+  //CAmbio de idioma
     document.querySelector(".espanol").innerHTML = "Espagnol";
     document.querySelector(".ingles").innerHTML = "Anglais";
     document.querySelector(".Frances").innerHTML = "Français";
 
-    // Menu hamburger
+     //Menu hamburguesa
     document.querySelector("#homeM").innerHTML = "Accueil";
     document.querySelector("#proyectosM").innerHTML = "Projets";
     document.querySelector("#sobreMiM").innerHTML = "À propos de moi";
     document.querySelector("#contactoL").innerHTML = "Contact";
-
-    // En-tête
+ 
+    //header
     document.querySelector(".home").innerHTML = "Accueil";
     document.querySelector(".proyectos").innerHTML = "Projets";
     document.querySelector(".sobreMi").innerHTML = "À propos de moi";
     document.querySelector(".contacto").innerHTML = "Contact";
 
-    // Principal
+    //principal
     document.querySelector(".saludo").innerHTML = "Bonjour à tous !!!, je suis";
 
-    // Projets
+    //proyectos
     document.querySelector(".proyectosText").innerHTML = "Projets";
     document.querySelector("#PokeAPI").innerHTML = "C'est un programme qui utilise la PokeAPI et simule le Pokédex, affichant les Pokémon avec leur numéro et leur nom. Il permet d'avancer ou de reculer un par un ou de les rechercher par numéro.";
 
@@ -145,30 +172,17 @@ function changeFrench() {
     <br><br>
     🚀 Mon objectif est de continuer à évoluer en tant que développeur, d'apporter de la valeur à des projets innovants et de collaborer avec des équipes dynamiques où je peux améliorer mes compétences et partager mes connaissances.`;
 
-    // Droits d'auteur
+    //derechos de autor
     document.querySelector(".derechos").innerHTML = " &copy; 2025 Luis Henao Campuslands. Tous droits réservés.";
 }
 
-var menuIdioma=0;
-document.querySelector(".iconIdiomaOculto").addEventListener("click", () => {
-    
-    if (menuIdioma===0) {
-        document.querySelector(".idiomasOculto").className="idiomasVisto"
-        document.querySelectorAll(".idiomasVisto p").forEach(element => {
-            element.addEventListener("click", () => {
-                document.querySelector(".idiomasVisto").className="idiomasOculto"
-                menuIdioma=0;
-            })
-        });
-        menuIdioma=1;
-        
-    } else {
-        document.querySelector(".idiomasVisto").className="idiomasOculto"
-        menuIdioma=0;
-        
-    }
-});
 
+
+document.querySelectorAll(".idiomasOculto p").forEach(element => {
+    element.addEventListener("click", () => {
+        document.querySelector(".idiomasOculto").open=false
+    })
+});
 
 
 document.querySelector(".espanol").addEventListener("click", () => {
