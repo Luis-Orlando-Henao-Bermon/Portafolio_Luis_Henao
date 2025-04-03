7539ab49e05fbfb4ab01515c2ead279d626d875d
Luis <roronoazoro3704@gmail.com>
2025-04-02 20:45:48 -0500

    feat: :sparkles: skills completed

--- main.js
+++ main.js
@@ -56,12 +56,15 @@ function changeSpanish() {
     document.querySelector("#proyectosM").innerHTML = "Proyectos";
     document.querySelector("#sobreMiM").innerHTML = "Sobre mí";
     document.querySelector("#contactoL").innerHTML = "Contacto";
+    document.querySelector("#habilidadesAM").innerHTML = "Habilidades";
 
     // Header
     document.querySelector(".home").innerHTML = "Inicio";
     document.querySelector(".proyectos").innerHTML = "Proyectos";
     document.querySelector(".sobreMi").innerHTML = "Sobre mí";
     document.querySelector(".contacto").innerHTML = "Contacto";
+    document.querySelector("#habilidadesA").innerHTML = "Habilidades";
+
 
     // Principal
     document.querySelector(".saludo").innerHTML = "¡Hola a todos!, soy";
@@ -74,12 +77,11 @@ function changeSpanish() {
 
     document.querySelector("#campus").innerHTML = "CampusLands ERP es una plataforma de simulación diseñada en Python para gestionar el proceso académico de los estudiantes en CampusLands. Permite registrar y hacer seguimiento a los estudiantes, asignarlos a rutas de aprendizaje, evaluar su desempeño y generar reportes de progreso de manera eficiente.";
 
-    document.querySelector("#superHero").innerHTML = "Esta aplicación web permite consultar información detallada de superhéroes utilizando la SuperHero API. Al ingresar el ID de un superhéroe (entre 1 y 732), la aplicación muestra en tablas organizadas los datos completos del personaje: nombre e imagen, estadísticas de poder (inteligencia, fuerza, velocidad, resistencia, poder y combate), biografía, apariencia física, ocupación, base de operaciones y conexiones con otros personajes. La interfaz presenta toda esta información de manera estructurada y visual para facilitar la consulta.";
+    document.querySelector("#superHero").innerHTML = "Happy Feets es una aplicación diseñada para la gestión de veterinarias, facilitando el control de citas, historial clínico, inventario y adopciones. Permite a clientes, veterinarios y administradores acceder a herramientas interactivas para mejorar la organización y atención de las mascotas.";
 
     document.querySelector(".AcercaDeMiText").innerHTML = "Sobre mí";
     document.querySelector(".infoMe").innerHTML = `💻 Soy Luis Henao, desarrollador fullstack junior apasionado por la tecnología y el aprendizaje constante. <br><br>
-    💡 Manejo lenguajes de programación como <b>JavaScript, Java, Python, HTML y CSS</b>. Para la gestión de bases de datos, trabajo con <b>MySQL y PostgreSQL</b>, y en frameworks utilizo <b>Spring Boot</b>.
-    <br><br>
+    
     👨‍💻 Me destaco por mi organización, capacidad de resolución de problemas y trabajo en equipo. Disfruto aprender nuevas tecnologías y mantenerme actualizado con las tendencias del desarrollo.
     <br><br>
     🚀 Mi objetivo es seguir creciendo como programador, aportar valor en proyectos innovadores y colaborar en equipos dinámicos donde pueda mejorar mis habilidades y compartir mis conocimientos.`;
@@ -98,6 +100,12 @@ function changeSpanish() {
     document.getElementById("message").placeholder="Mensaje"
     document.getElementById("btn").value="Enviar"
 
+    document.querySelector(".HabilidadesText").innerHTML = "Habilidades";
+    document.getElementById("problemText").value="Resolución de problemas"
+    document.getElementById("teamText").value="Trabajo en equipo"
+    document.getElementById("organizationText").value="Organización"
+    document.getElementById("learningText").value="learningText"
+
     // Derechos de autor
     document.querySelector(".derechos").innerHTML = " &copy; 2025 Luis Henao Campuslands. Todos los derechos reservados.";
 }
@@ -114,12 +122,15 @@ function changeEnglish() {
     document.querySelector("#proyectosM").innerHTML="Projects "
     document.querySelector("#sobreMiM").innerHTML="About me "
     document.querySelector("#contactoL").innerHTML="Contact"
+    document.querySelector("#habilidadesAM").innerHTML = "Skills";
+
     
     //header
     document.querySelector(".home").innerHTML="Home"
     document.querySelector(".proyectos").innerHTML="Projects "
     document.querySelector(".sobreMi").innerHTML="About me"
     document.querySelector(".contacto").innerHTML="Contact"
+    document.querySelector("#habilidadesA").innerHTML = "Skills";
     
     //principal
     document.querySelector(".saludo").innerHTML="Hello everyone!!!, I am"
@@ -132,12 +143,11 @@ function changeEnglish() {
     
     document.querySelector("#campus").innerHTML="CampusLands ERP is a simulation platform designed in Python to manage the academic process of students at CampusLands. It allows for student registration and tracking, assignment to learning paths, performance evaluation, and efficient progress report generation."
     
-    document.querySelector("#superHero").innerHTML="CThis web application allows users to retrieve detailed information about superheroes using the SuperHero API. By entering a superhero ID (between 1 and 732), the application displays the character’s complete data in organized tables, including name and image, power statistics (intelligence, strength, speed, durability, power, and combat), biography, physical appearance, occupation, base of operations, and connections with other characters. The interface presents all this information in a structured and visual manner for easy reference."
+    document.querySelector("#superHero").innerHTML="Happy Feets is an application designed for veterinary management, facilitating the control of appointments, medical records, inventory, and adoptions. It allows clients, veterinarians, and administrators to access interactive tools to improve organization and pet care."
     
     document.querySelector(".AcercaDeMiText").innerHTML="About me"
     document.querySelector(".infoMe").innerHTML=`💻 I’m Luis Henao, a junior full-stack developer passionate about technology and continuous learning. <br><br>
-    💡 I work with programming languages such as <b>JavaScript, Java, Python, HTML, and CSS</b>. For database management, I use <b>MySQL and PostgreSQL</b>, and I work with frameworks like <b>Spring Boot</b>.
-    <br><br>
+    
     👨‍💻 I stand out for my organization, problem-solving skills, and teamwork. I enjoy learning new technologies and staying up to date with development trends.
     <br><br>
     🚀 My goal is to keep growing as a developer, contribute to innovative projects, and collaborate with dynamic teams where I can improve my skills and share my knowledge.`
@@ -155,6 +165,13 @@ function changeEnglish() {
     document.getElementById("message").placeholder = "Message";
     document.getElementById("btn").value = "Send";
 
+    document.querySelector(".HabilidadesText").innerHTML = "Skills";
+    document.getElementById("problemText").innerHTML = "Problem Solving";
+    document.getElementById("teamText").innerHTML = "Teamwork";
+    document.getElementById("organizationText").innerHTML = "Organization";
+    document.getElementById("learningText").innerHTML = "Continuous Learning";
+
+
     //derechos de autor
     document.querySelector(".derechos").innerHTML=" &copy; 2025 Luis Henao Campuslands. All rights reserved."
 
@@ -171,12 +188,15 @@ function changeFrench() {
     document.querySelector("#proyectosM").innerHTML = "Projets";
     document.querySelector("#sobreMiM").innerHTML = "À propos de moi";
     document.querySelector("#contactoL").innerHTML = "Contact";
- 
+    document.querySelector("#habilidadesAM").innerHTML = "Compétences";
+    
+    
     //header
     document.querySelector(".home").innerHTML = "Accueil";
     document.querySelector(".proyectos").innerHTML = "Projets";
     document.querySelector(".sobreMi").innerHTML = "À propos de moi";
     document.querySelector(".contacto").innerHTML = "Contact";
+    document.querySelector("#habilidadesA").innerHTML = "Compétences";
 
     //principal
     document.querySelector(".saludo").innerHTML = "Bonjour à tous !!!, je suis";
@@ -189,12 +209,11 @@ function changeFrench() {
 
     document.querySelector("#campus").innerHTML = "CampusLands ERP est une plateforme de simulation conçue en Python pour gérer le processus académique des étudiants de CampusLands. Elle permet d'enregistrer et de suivre les étudiants, de les affecter à des parcours d'apprentissage, d'évaluer leurs performances et de générer des rapports de progression de manière efficace.";
 
-    document.querySelector("#superHero").innerHTML = "Cette application web permet de consulter des informations détaillées sur les super-héros en utilisant l'API SuperHero. En entrant l'ID d'un super-héros (entre 1 et 732), l'application affiche les données complètes du personnage dans des tableaux organisés, y compris le nom et l'image, les statistiques de pouvoir (intelligence, force, vitesse, endurance, pouvoir et combat), la biographie, l'apparence physique, la profession, la base d'opérations et les connexions avec d'autres personnages. L'interface présente toutes ces informations de manière structurée et visuelle pour faciliter la consultation.";
+    document.querySelector("#superHero").innerHTML = "Happy Feets est une application conçue pour la gestion des cliniques vétérinaires, facilitant le contrôle des rendez-vous, des dossiers médicaux, de l'inventaire et des adoptions. Elle permet aux clients, aux vétérinaires et aux administrateurs d'accéder à des outils interactifs pour améliorer l'organisation et les soins aux animaux.";
 
     document.querySelector(".AcercaDeMiText").innerHTML = "À propos de moi";
     document.querySelector(".infoMe").innerHTML = `💻 Je suis Luis Henao, un développeur fullstack junior passionné par la technologie et l'apprentissage continu. <br><br>
-    💡 Je maîtrise des langages de programmation comme <b>JavaScript, Java, Python, HTML et CSS</b>. Pour la gestion des bases de données, j'utilise <b>MySQL et PostgreSQL</b>, et je travaille avec des frameworks comme <b>Spring Boot</b>.
-    <br><br>
+    
     👨‍💻 Je me distingue par mon organisation, ma capacité à résoudre des problèmes et mon travail en équipe. J'aime apprendre de nouvelles technologies et me tenir à jour avec les tendances du développement.
     <br><br>
     🚀 Mon objectif est de continuer à évoluer en tant que développeur, d'apporter de la valeur à des projets innovants et de collaborer avec des équipes dynamiques où je peux améliorer mes compétences et partager mes connaissances.`;
@@ -212,6 +231,12 @@ function changeFrench() {
     document.getElementById("messageLabel").innerHTML = "Message";
     document.getElementById("message").placeholder = "Message";
     document.getElementById("btn").value = "Envoyer";
+    document.querySelector(".HabilidadesText").innerHTML = "Compétences";
+    document.getElementById("problemText").innerHTML = "Résolution de problèmes";
+    document.getElementById("teamText").innerHTML = "Travail d'équipe";
+    document.getElementById("organizationText").innerHTML = "Organisation";
+    document.getElementById("learningText").innerHTML = "Apprentissage continu";
+
 
 
     //derechos de autor
